c48561e32ab6edacb891f0a5f54c7dfc2c314ba4
livelton <elivelton.pereira@academico.ifpb.edu.br>
2024-09-25 17:09:25 -0300

    Aplicando responsividade em gerenciar e navbar

--- src/components/Button/ButtonTab.tsx
+++ src/components/Button/ButtonTab.tsx
@@ -12,7 +12,7 @@ const ButtonTab = (props: InputProps) => {
         <>
             <button
                 onClick={props.onClick}
-                className={`flex items-center gap-2 w-[15vw] justify-center p-2 px-3 font-bold text-lg hover:bg-primaria-light hover:text-white transition-all duration-300 ${
+                className={`flex items-center gap-2 lg:w-[15vw] md:w-screen xsm:w-screen justify-center md:p-4 xsm:p-4 lg:p-2 px-3 font-bold text-lg hover:bg-primaria-light hover:text-white transition-all duration-300 ${
                     props.color == "primaria" ? "bg-primaria text-white" : "bg-secundaria"
                 } rounded-lg`}
             >
